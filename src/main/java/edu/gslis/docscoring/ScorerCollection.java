0a109e55e1a4ceb558a4233b6bccf0dda96c72e4
Garrick Sherman <gsherma2@illinois.edu>
2016-02-29 11:05:12 -0600

    probably more efficient

--- src/main/java/edu/gslis/docscoring/ScorerCollection.java
+++ src/main/java/edu/gslis/docscoring/ScorerCollection.java
@@ -1,12 +1,30 @@
 package edu.gslis.docscoring;
 
-public class ScorerCollection extends ScorerJelinekMercer {
+import java.util.Iterator;
+
+import edu.gslis.queries.GQuery;
+import edu.gslis.searchhits.SearchHit;
+
+public class ScorerCollection extends QueryDocScorer {
 	
-	public ScorerCollection() {
-		/* Simply reuse a Jelinek-Mercer scorer (through inheritance) with
-		 * all the weight on the collection probability.
-		 */
-		setParameter(MIXING_PARAM, 1.0);
+	private double epsilon = 1.0;
+	
+	public void setQuery(GQuery query) {
+		this.gQuery = query;
+	}
+	
+	public double score(SearchHit doc) {
+		double logLikelihood = 0.0;
+		Iterator<String> termIterator = gQuery.getFeatureVector().iterator();
+		while (termIterator.hasNext()) {
+			String term = termIterator.next();
+			
+			double collectionProb = (epsilon + collectionStats.termCount(term)) / collectionStats.getTokCount();
+			
+			double queryWeight = gQuery.getFeatureVector().getFeatureWeight(term);
+			logLikelihood += queryWeight*Math.log(collectionProb);
+		}
+		return logLikelihood;
 	}
 
 }
